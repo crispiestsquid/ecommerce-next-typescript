17772a6e018f5ba5193576b55425cfd81b513405
Kevin Little <crispiestsquid@aol.com>
2022-01-16 13:20:19 -0500

    typescript intro done

--- playground.ts
+++ playground.ts
@@ -1,11 +1,24 @@
-interface Person {
-    name: string,
-    age: number,
-    grades: number[]
-}
-type PersonKey = keyof Person;
+export default async function play() {
+    type Greeting = { message: string };
+    type InferHelloProps<T> = T extends () => Promise<{ props: infer Props }> ? Props : never;
 
+    const getHelloProps = async () => {
+        const greeting: Greeting = { message: "Hi friends!" };
 
-export default function play() {
-    const personKey: PersonKey = "grades";
+        return {
+            props: {
+                greeting,
+                data: {
+                    cars: ["car1", "car2"]
+                }
+            }
+        };
+    }
+
+    function sayHello(props: InferHelloProps<typeof getHelloProps>) {
+        console.log(props.greeting.message);
+    }
+
+    const data = await getHelloProps();
+    sayHello(data.props);
 }
